7d49fb58236171864d7a1680cc31208b8b542e65
Justin Siek <justinsiek05@gmail.com>
2024-12-23 15:25:53 -0800

    connecting backend to frontend

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "express";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
