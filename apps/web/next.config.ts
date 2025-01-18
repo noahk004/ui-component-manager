590d6f02892997df7b7a0c6f2ce913173b98c480
Justin Siek <justinsiek05@gmail.com>
2025-01-18 12:04:47 -0800

    added extract plugin

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    webpack: (config) => {
+        config.plugins.push(new MiniCssExtractPlugin());
+        return config;
+    }
 };
 
 export default nextConfig;
